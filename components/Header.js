37d6408bf7b796987ff6c4b43966c801b7f96a95
Roman <didoshakr@ukr.net>
2020-03-03 22:10:41 +0200

    F

--- components/Header.js
+++ components/Header.js
@@ -1,43 +1,77 @@
 //Header.js
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+
 import Logo from "./Logo";
 import HeaderMenu from "./HeaderMenu";
 import { ComponentContext } from "../context/ComponentContext";
 // import DrawerButton from "./Navigation/DrawerBootton";
 // import Drawer from "./Navigation/Drawer";
+// import { faColumns } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   const { state } = useContext(ComponentContext);
   const theme = state.theme;
-  const [drawerOpen, setDrawerOpen] = React.useState(false);
-  const drawerOnClick = () => {
-    setDrawerOpen(!drawerOpen);
-  };
+  // const [drawerOpen, setDrawerOpen] = React.useState(false);
+  const [headerOpen, setHeaderOpen] = React.useState(true);
+  // const drawerOnClick = () => {
+  //   setDrawerOpen(!drawerOpen);
+  // };
+
+  //***** щоб ховалась/появлялась Head при скролі ********/
+  var prevScrollpos = 0;
+  function mouseMove() {
+    var currentScrollPos = pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      setHeaderOpen(true);
+    } else {
+      setHeaderOpen(false);
+    }
+    prevScrollpos = currentScrollPos;
+  }
+
+  useEffect(() => {
+    // Прив’яжіть прослуховувач події
+    document.addEventListener("scroll", mouseMove); //Для скролу
+    // document.addEventListener("mousemove", mouseMove);//для переміщення мишки (координати)
+    return () => {
+      // Від’єднайте слухача події під час очищення
+      document.removeEventListener("scroll", mouseMove);
+      // document.removeEventListener("mousemove", mouseMove);
+    };
+  });
+
   return (
     <div className="header-fixed">
+      {/* <Drawer drawerOpen={drawerOpen} drawerOnClick={drawerOnClick} />
+      <DrawerButton drawerOpen={drawerOpen} drawerOnClick={drawerOnClick} /> */}
       <div className="header-wrapper">
-        {/* <Drawer drawerOpen={drawerOpen} drawerOnClick={drawerOnClick} /> */}
-        {/* <DrawerButton drawerOpen={drawerOpen} drawerOnClick={drawerOnClick} /> */}
         <Logo />
         <HeaderMenu />
       </div>
+
       <style jsx>{`
         .header-fixed {
           position: fixed;
-          top: 0;
+          top: ${headerOpen ? "0" : "-60px"};
+          transition: top 0.4s ease-in;
           height: 60px;
           width: 100%;
           z-index: 100;
         }
         .header-wrapper {
-          padding: 10px; //Відступ зверху
+          padding: 10px; /*Відступ зверху*/
+          max-height: 60px;
           display: flex;
-          justify-content: space-between; //Вирівнювання вправо
+          justify-content: space-between; /*Вирівнювання вправо*/
           align-items: center;
         }
-        @media (max-width: 600px) {
-          display: block;
-          text-align: center;
+        @media (max-width: 1200px) {
+          /*@media (max-width: 600px) {*/
+          .header-fixed {
+            position: relative;
+            /*display: block;*/
+            text-align: center;
+          }
         }
       `}</style>
       <style jsx>{`
--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,10 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
           {/* //https://www.youtube.com/watch?v=GfsqFaiaK3A */}
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
           <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         </Head>
         <body>
