de170184f12d2644027715e249f31d887698e334
Roman <didoshakr@ukr.net>
2020-03-03 22:32:23 +0200

    F

--- components/Header.js
+++ components/Header.js
@@ -8,6 +8,9 @@ import { ComponentContext } from "../context/ComponentContext";
 // import Drawer from "./Navigation/Drawer";
 // import { faColumns } from "@fortawesome/free-solid-svg-icons";
 
+var lastScrollTop = 0;
+var scrolUP = false;
+
 const Header = () => {
   const { state } = useContext(ComponentContext);
   const theme = state.theme;
@@ -17,17 +20,41 @@ const Header = () => {
   //   setDrawerOpen(!drawerOpen);
   // };
 
-  //***** щоб ховалась/появлялась Head при скролі ********/
-  var prevScrollpos = 0;
+  //*** щоб ховалась/появлялась Head при скролі
   function mouseMove() {
-    var currentScrollPos = pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      setHeaderOpen(true);
+    // alert("Ти порухав мишкою!/mousemove");
+    // console.log("Header.js/скрол=", pageYOffset);
+    // console.log("Header.js/Y=", event.pageY);
+    var st = pageYOffset;
+    console.log("Header.js/st=", st + "/lastScrollTop=", lastScrollTop);
+    if (st > 100) {
+      if (st - lastScrollTop > 0 || (st - lastScrollTop == 0 && !scrolUP)) {
+        console.log("Скрол вниз!");
+        scrolUP = false;
+        // alert("Скрол вниз!");
+        setHeaderOpen(false);
+        // downscroll code
+      } else {
+        setHeaderOpen(true);
+        scrolUP = true;
+        console.log("Скрол вверх!");
+      }
     } else {
-      setHeaderOpen(false);
+      setHeaderOpen(true);
     }
-    prevScrollpos = currentScrollPos;
+    lastScrollTop = st;
   }
+
+  // var prevScrollpos = window.pageYOffset;
+  // function mouseMove() {
+  //   var currentScrollPos = window.pageYOffset;
+  //   if (prevScrollpos > currentScrollPos) {
+  //     setHeaderOpen(true);
+  //   } else {
+  //     setHeaderOpen(false);
+  //   }
+  //   prevScrollpos = currentScrollPos;
+  // }
 
   useEffect(() => {
     // Прив’яжіть прослуховувач події
@@ -53,9 +80,9 @@ const Header = () => {
         .header-fixed {
           position: fixed;
           top: ${headerOpen ? "0" : "-60px"};
-          transition: top 0.4s ease-in;
           height: 60px;
           width: 100%;
+          transition: top 0.4s ease-in;
           z-index: 100;
         }
         .header-wrapper {
@@ -64,14 +91,6 @@ const Header = () => {
           display: flex;
           justify-content: space-between; /*Вирівнювання вправо*/
           align-items: center;
-        }
-        @media (max-width: 1200px) {
-          /*@media (max-width: 600px) {*/
-          .header-fixed {
-            position: relative;
-            /*display: block;*/
-            text-align: center;
-          }
         }
       `}</style>
       <style jsx>{`
